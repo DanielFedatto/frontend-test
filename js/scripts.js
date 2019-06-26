b8f098037befe86e251512361b0ad7630b17d624
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-06-26 18:03:46 -0300

    Desenvolvimento do corpo do site

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
-    $('#Slider').slick({
+    // Slider dos Banners
+    $('#banner-slider').slick({
         dots: true,
         arrows: false,
         draggable: false,
+    });
+    // Slider da Galeria
+    $('#gallery-slider').slick ({
+        dots: false,
+        arrows: true,
+        draggable: false,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+    // Função da lista de Accordions
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+
+        e.preventDefault();
     });
 });
